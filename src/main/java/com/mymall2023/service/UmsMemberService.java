f1bbdb12f463c12e9e649dadaf4099b859655144
zyang1007 <willzhu0411@gmail.com>
2023-05-17 13:42:18 -0700

    configure Redis

---
+++ src/main/java/com/mymall2023/service/UmsMemberService.java
@@ -0,0 +1,20 @@
+package com.mymall2023.service;
+
+import com.mymall2023.common.api.CommonResult;
+
+/**
+ * 会员管理Service
+ */
+public interface UmsMemberService {
+
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+
+}
