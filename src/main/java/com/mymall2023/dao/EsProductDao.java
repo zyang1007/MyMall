8188ee73cf250053b66326dc446774fb57f5a9c6
zyang1007 <willzhu0411@gmail.com>
2023-05-23 15:18:10 -0700

    configure ES

---
+++ src/main/java/com/mymall2023/dao/EsProductDao.java
@@ -0,0 +1,13 @@
+package com.mymall2023.dao;
+
+import com.mymall2023.nosql.elasticsearch.document.EsProduct;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 搜索系统中的商品管理自定义Dao
+ */
+public interface EsProductDao {
+    List<EsProduct> getAllEsProductList(@Param("id") Long id);
+}
